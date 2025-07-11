70d9ee4017b4a186d3198dae980ffbe31b8df649
MaxBrainygame <maksim.gulevich.97@gmail.com>
2025-07-11 13:56:35 +0400

    add animation player

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +12,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:		
 		velocity.x = direction * SPEED
-	else:
+	else:	
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+	if velocity.y:
+		animated_sprite_2d.play("jump")
+	elif velocity.x:
+		animated_sprite_2d.play("walk")
+	else:
+		animated_sprite_2d.play("idle")
+	animated_sprite_2d.flip_h = direction < 0
 
 	move_and_slide()
